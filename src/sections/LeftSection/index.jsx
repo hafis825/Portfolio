732101209964026de7239f2419bc3e2734f89854
hafis825 <150876306+hafis825@users.noreply.github.com>
2024-12-08 15:52:02 +0700

    Update index.jsx

--- src/sections/LeftSection/index.jsx
+++ src/sections/LeftSection/index.jsx
@@ -1,6 +1,6 @@
-import Contact from "../contact"
+import Contact from "../Contact"
 import Header from "../Header"
-import Navbar from "../navbar"
+import Navbar from "../Navbar"
 
 
 const LeftSection = ({ navBarItems, currentSection }) =>{
